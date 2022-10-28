c5a4da36459e1bf33567261315e3045095fafac0
James Easton <james.easton@qa.com>
2022-10-28 10:32:38 +0100

    Updated availability_zone syntax for ec2 configuration

--- main_ec2.tf
+++ main_ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "wp_instance" {
   ami               = "ami-0648ea225c13e0729"
   instance_type     = "t2.micro"
-  availability_zone = data.aws_availability_zones.available
+  availability_zone = data.aws_availability_zones.available.names[0]
   subnet_id         = aws_subnet.public_1_subnet.id
   user_data         = "./scripts/userdata.txt"
 
