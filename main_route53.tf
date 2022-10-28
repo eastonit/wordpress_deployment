61d552ac24579e7dfb6ff0d633474aa5569155b5
James Easton <james.easton@qa.com>
2022-10-28 11:55:36 +0100

    Removed NS record from route53 configuration

--- main_route53.tf
+++ main_route53.tf
@@ -3,11 +3,3 @@ resource "aws_route53_zone" "wp_domain" {
 
 }
 
-resource "aws_route53_record" "wp_domain_ns_record" {
-  zone_id = aws_route53_zone.wp_domain.zone_id
-  name    = "eastonit.org"
-  type    = "NS"
-  ttl     = "30"
-  records = aws_route53_zone.wp_domain.name_servers
-
-}
