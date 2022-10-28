d47370baec0cd707b4e7e6f364acecea5ac5ee22
James Easton <james.easton@qa.com>
2022-10-28 11:12:02 +0100

    Extended password for rds as too short

--- main_rds.tf
+++ main_rds.tf
@@ -4,8 +4,8 @@ resource "aws_db_instance" "wp_database" {
   engine               = "mysql"
   engine_version       = "8.0.28"
   instance_class       = "db.t3.micro"
-  username             = "admin"
-  password             = "admin"
+  username             = "adminadmin"
+  password             = "adminadmin"
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
 }
