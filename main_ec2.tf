6dfaacc231d8a4b4a05eb938f166681ffb4e434f
James Easton <james.easton@qa.com>
2022-10-28 11:00:57 +0100

    Updated subnet id syntax

--- main_ec2.tf
+++ main_ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "wp_instance" {
   ami           = "ami-0648ea225c13e0729"
   instance_type = "t2.micro"
-  subnet_id     = aws.subnet_id.public_1_subnet
+  subnet_id     = aws.subnet_id.public_1_subnet.subnet_id
   user_data     = "./scripts/userdata.txt"
 
 }
